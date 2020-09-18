bb7d47a4dd4569173e1e3a1b68c8b6771a800823
Kohpai <whcwhc78@gmail.com>
2020-09-18 13:29:39 +0200

    ✨ add Link in nav bar

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Button, Drawer, Menu } from "antd";
 import { MenuUnfoldOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   const [drawerVisibility, setVisibility] = useState(false);
@@ -25,8 +26,12 @@ export const NavBar = () => {
         visible={drawerVisibility}
       >
         <Menu mode="vertical" theme="dark">
-          <Menu.Item key="1">Get started guide</Menu.Item>
-          <Menu.Item key="2">What's this?</Menu.Item>
+          <Menu.Item key="1">
+            <Link to="/">Get started guide</Link>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <Link to="/about">What's this?</Link>
+          </Menu.Item>
         </Menu>
       </Drawer>
     </>
