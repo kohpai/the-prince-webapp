2671e39e603b58f162fa5e23a61e54e3f7dbfd44
Kohpai <whcwhc78@gmail.com>
2020-09-26 22:31:10 +0200

    ✨ submit print job

--- src/components/JobRequestForm.tsx
+++ src/components/JobRequestForm.tsx
@@ -7,6 +7,8 @@ import {
   Select,
   Input,
   Typography,
+  Modal,
+  message,
 } from "antd";
 import { UploadFile } from "antd/lib/upload/interface";
 import {
@@ -18,6 +20,8 @@ import firebase from "firebase";
 import Big from "big.js";
 
 import config from "../config";
+import { gql, useMutation } from "@apollo/client";
+import { Loading } from "./Loading";
 
 interface PrintConfig {
   colorMode: "BLACK" | "COLOR";
@@ -33,6 +37,23 @@ interface PrintPrice {
 
 const { Option } = Select;
 const { Title } = Typography;
+
+const SUBMIT_PRINT_JOB = gql`
+  mutation SubmitPrintJob(
+    $filename: String!
+    $printConfig: PrintConfigTInput!
+  ) {
+    submitPrintJob(input: { filename: $filename, printConfig: $printConfig }) {
+      printJob {
+        id
+        createdAt
+        customer {
+          balance
+        }
+      }
+    }
+  }
+`;
 
 function calculatePrintPrice(pc: PrintConfig): Big {
   const numPages = pc.pageRange ? parsePageRange(pc.pageRange) : pc.numPages;
@@ -83,11 +104,31 @@ export const JobRequestForm = () => {
     },
     price: new Big(0),
   });
+  const [loading, setLoading] = useState(false);
 
-  const onFinish = (values: any) => {
-    console.log("kohpai-finished", values);
+  const [submitPrintJob] = useMutation(SUBMIT_PRINT_JOB);
+
+  const onFinish = async () => {
+    setLoading(true);
+
+    const { data, errors } = await submitPrintJob({
+      variables: {
+        filename: fileList[0].name,
+        printConfig: printPrice.config,
+      },
+    });
+
+    setLoading(false);
+
+    if (errors) {
+      message.error(`Failed to sumbit the print job: ${errors[0].message}`, 0);
+    } else {
+      message.success(
+        `Job ID ${data.submitPrintJob.printJob.id} is submitted`,
+        3
+      );
+    }
   };
-  const onFinishFailed = () => {};
 
   firebase
     .auth()
@@ -96,135 +137,148 @@ export const JobRequestForm = () => {
     );
 
   return (
-    <Form
-      labelCol={{ md: 8 }}
-      wrapperCol={{ md: 16 }}
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      colon={false}
-    >
-      <Form.Item
-        label="PDF file"
-        name="filename"
-        rules={[{ required: true, message: "Please upload a PDF file!" }]}
+    <>
+      <Form
+        labelCol={{ md: 8 }}
+        wrapperCol={{ md: 16 }}
+        name="basic"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        colon={false}
       >
-        <Upload
-          fileList={fileList}
-          accept="application/pdf"
-          action={`${config.API_URL}/upload`}
-          headers={{ Authorization: `Bearer ${jwt}` }}
-          beforeUpload={async (_) => {
-            user && setJWT(await user.getIdToken());
-          }}
-          onChange={({ fileList, file }) => {
-            updateFileList(fileList.filter((file) => !!file.status).slice(-1));
-
-            const { response } = file;
-            if (response) {
-              const printConfig = {
-                ...printPrice.config,
-                numPages: response.numPages,
-              };
-              setPrincePrice({
-                config: printConfig,
-                price: calculatePrintPrice(printConfig),
-              });
-            }
-          }}
+        <Form.Item
+          label="PDF file"
+          name="filename"
+          rules={[{ required: true, message: "Please upload a PDF file!" }]}
         >
-          <Button icon={<UploadOutlined />}>Upload your document</Button>
-        </Upload>
-      </Form.Item>
+          <Upload
+            fileList={fileList}
+            accept="application/pdf"
+            action={`${config.API_URL}/upload`}
+            headers={{ Authorization: `Bearer ${jwt}` }}
+            beforeUpload={async (_) => {
+              user && setJWT(await user.getIdToken());
+            }}
+            onChange={({ fileList, file }) => {
+              updateFileList(
+                fileList.filter((file) => !!file.status).slice(-1)
+              );
 
-      <Form.Item label="Color mode" name="colorMode">
-        <Select
-          defaultValue="BLACK"
-          onChange={(value) => {
-            const printConfig = { ...printPrice.config, colorMode: value };
-            setPrincePrice({
-              config: printConfig,
-              price: calculatePrintPrice(printConfig),
-            });
-          }}
-        >
-          <Option value="BLACK">Black {"&"} White</Option>
-          <Option value="COLOR">Color</Option>
-        </Select>
-      </Form.Item>
-
-      <Form.Item
-        label="Page range"
-        name="pageRange"
-        rules={[
-          (_) => ({
-            validator: (_, value) => {
-              if (!value) {
-                return Promise.resolve();
+              const { response } = file;
+              if (response) {
+                const printConfig = {
+                  ...printPrice.config,
+                  numPages: response.numPages,
+                };
+                setPrincePrice({
+                  config: printConfig,
+                  price: calculatePrintPrice(printConfig),
+                });
               }
+            }}
+          >
+            <Button icon={<UploadOutlined />}>Upload your document</Button>
+          </Upload>
+        </Form.Item>
 
-              return new Promise((resolve) => {
-                parsePageRange(value);
-                resolve();
+        <Form.Item label="Color mode" name="colorMode">
+          <Select
+            defaultValue="BLACK"
+            onChange={(value) => {
+              const printConfig = { ...printPrice.config, colorMode: value };
+              setPrincePrice({
+                config: printConfig,
+                price: calculatePrintPrice(printConfig),
               });
-            },
-          }),
-        ]}
-      >
-        <Input
-          placeholder="1,3,5-9,12"
-          onChange={(e) => {
-            try {
-              const value = e.currentTarget.value;
+            }}
+          >
+            <Option value="BLACK">Black {"&"} White</Option>
+            <Option value="COLOR">Color</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          label="Page range"
+          name="pageRange"
+          rules={[
+            (_) => ({
+              validator: (_, value) => {
+                if (!value) {
+                  return Promise.resolve();
+                }
+
+                return new Promise((resolve) => {
+                  parsePageRange(value);
+                  resolve();
+                });
+              },
+            }),
+          ]}
+        >
+          <Input
+            placeholder="1,3,5-9,12"
+            onChange={(e) => {
+              try {
+                const value = e.currentTarget.value;
+                const printConfig = {
+                  ...printPrice.config,
+                  pageRange: value || undefined,
+                };
+                setPrincePrice({
+                  config: printConfig,
+                  price: calculatePrintPrice(printConfig),
+                });
+              } catch {}
+            }}
+          />
+        </Form.Item>
+
+        <Form.Item label="Copies" name="numCopies">
+          <InputNumber
+            min={1}
+            defaultValue={1}
+            onChange={(value) => {
               const printConfig = {
                 ...printPrice.config,
-                pageRange: value,
+                numCopies: +(value || 1),
               };
               setPrincePrice({
                 config: printConfig,
                 price: calculatePrintPrice(printConfig),
               });
-            } catch {}
-          }}
-        />
-      </Form.Item>
+            }}
+          />
+        </Form.Item>
 
-      <Form.Item label="Copies" name="numCopies">
-        <InputNumber
-          min={1}
-          defaultValue={1}
-          onChange={(value) => {
-            const printConfig = {
-              ...printPrice.config,
-              numCopies: +(value || 1),
-            };
-            setPrincePrice({
-              config: printConfig,
-              price: calculatePrintPrice(printConfig),
-            });
-          }}
-        />
-      </Form.Item>
+        <Form.Item
+          style={{ fontWeight: "bold" }}
+          label={<Title level={5}>You'll be charged</Title>}
+          name="price"
+        >
+          <Title level={5}>{printPrice.price.toString()} €</Title>(
+          {printPrice.config.pageRange
+            ? parsePageRange(printPrice.config.pageRange)
+            : printPrice.config.numPages}{" "}
+          pages, {printPrice.config.numCopies} copies)
+        </Form.Item>
 
-      <Form.Item
-        style={{ fontWeight: "bold" }}
-        label={<Title level={5}>You'll be charged</Title>}
-        name="price"
+        <Form.Item label={<PlayCircleTwoTone />}>
+          <Button type="primary" htmlType="submit">
+            <PrinterOutlined />
+            Print
+          </Button>
+        </Form.Item>
+      </Form>
+      <Modal
+        className="undo-info"
+        title="Submitting print job"
+        visible={loading}
+        okButtonProps={{ hidden: true }}
+        cancelButtonProps={{ hidden: true }}
+        closable={false}
       >
-        <Title level={5}>{printPrice.price.toString()} €</Title>(
-        {printPrice.config.pageRange
-          ? parsePageRange(printPrice.config.pageRange)
-          : printPrice.config.numPages}{" "}
-        pages, {printPrice.config.numCopies} copies)
-      </Form.Item>
-
-      <Form.Item label={<PlayCircleTwoTone />}>
-        <Button type="primary" htmlType="submit">
-          <PrinterOutlined />
-          Print
-        </Button>
-      </Form.Item>
-    </Form>
+        <Loading text="Please wait while we're submitting your print job." />
+      </Modal>
+    </>
   );
 };
